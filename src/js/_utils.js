f7ca146a4c505108f342b618f4ec9982014e1a93
Charlie <charlie@makedo.net>
2020-08-11 15:06:36 +0100

    example tweaks

--- src/js/_utils.js
+++ src/js/_utils.js
@@ -1,6 +1,6 @@
-function deBounce(func, wait = 100) {
+export function deBounce(func, wait = 100) {
 	let timeout;
-	return function(...args) {
+	return function (...args) {
 		clearTimeout(timeout);
 		timeout = setTimeout(() => {
 			func.apply(this, args);
@@ -10,26 +10,26 @@ function deBounce(func, wait = 100) {
 
 /*! track-focus v 1.0.0 | Author: Jeremy Fields [jeremy.fields@vget.com], 2015 | License: MIT */
 // inspired by: http://irama.org/pkg/keyboard-focus-0.3/jquery.keyboard-focus.js
-function trackFocus(body) {
+export function trackFocus(body) {
 	var usingMouse;
 
-	var preFocus = function(event) {
-		usingMouse = "mousedown" === event.type;
+	var preFocus = function (event) {
+		usingMouse = 'mousedown' === event.type;
 	};
 
-	var addFocus = function(event) {
-		if (usingMouse) event.target.classList.add("focus--mouse");
+	var addFocus = function (event) {
+		if (usingMouse) event.target.classList.add('focus--mouse');
 	};
 
-	var removeFocus = function(event) {
-		event.target.classList.remove("focus--mouse");
+	var removeFocus = function (event) {
+		event.target.classList.remove('focus--mouse');
 	};
 
-	var bindEvents = function() {
-		body.addEventListener("keydown", preFocus);
-		body.addEventListener("mousedown", preFocus);
-		body.addEventListener("focusin", addFocus);
-		body.addEventListener("focusout", removeFocus);
+	var bindEvents = function () {
+		body.addEventListener('keydown', preFocus);
+		body.addEventListener('mousedown', preFocus);
+		body.addEventListener('focusin', addFocus);
+		body.addEventListener('focusout', removeFocus);
 	};
 
 	bindEvents();
